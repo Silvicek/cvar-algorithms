b569847523cd0e2d879b7b5af11cb422d8f0867f
Silvestr Stanko <stanksil@fel.cvut.cz>
2017-10-12 20:25:03 +0200

    alpha-based improvement works

--- distributional.py
+++ distributional.py
@@ -1,9 +1,9 @@
 from cliffwalker import *
 import matplotlib.pyplot as plt
 from visual import show_results
-from exp_model import greedy_policy
+import exp_model
 
-REWARD_RANGE = (FALL_REWARD-10, 1)
+REWARD_RANGE = (FALL_REWARD-20, 1)
 
 
 class RandomVariable:
@@ -18,6 +18,46 @@ class RandomVariable:
 
     def expected_value(self):
         return np.dot(self.z, self.p)
+
+    def cvar(self, alpha):
+        if alpha > 0.999:
+            return self.expected_value()
+        p = 0.
+        i = 0
+        while p < alpha:
+            p += self.p[i]
+            i += 1
+        i -= 1  # TODO: check
+        p = p - self.p[i]
+        p_rest = alpha - p
+        cvar = (np.dot(self.p[:i], self.z[:i]) + p_rest * self.z[i]) / alpha
+
+        return cvar
+
+    def var(self, alpha):
+        if alpha > 0.999:
+            return self.z[-1]
+        p = 0.
+        i = 0
+        while p < alpha:
+            p += self.p[i]
+            i += 1
+
+        if p == alpha:
+            # TODO: check
+            # print('Miracle')
+            var = self.z[i-1]
+        else:
+            var = self.z[i]
+
+        return var
+
+    def alpha_from_var(self, var):
+        # TODO: deal with discrete distributions
+        i, = np.where(self.z == var)
+        # print(var, i, self.z)
+
+        return np.sum(self.p[:i[0]+1])
 
     def __add__(self, r):
         # uses the fact that rewards are all negative ints
@@ -48,6 +88,94 @@ class RandomVariable:
         plt.show()
 
 
+def cvar_from_samples(samples, alpha):
+    samples = np.sort(samples)
+    alpha_ix = int(np.round(alpha * len(samples)))
+    var = samples[alpha_ix - 1]
+    cvar = np.mean(samples[:alpha_ix])
+    return var, cvar
+
+
+class Policy:
+    """ Abstract class representing different policies. """
+
+    __name__ = 'Policy'
+
+    def next_action(self, s, r):
+        raise NotImplementedError()
+
+    def reset(self):
+        raise NotImplementedError()
+
+
+class GreedyPolicy(Policy):
+    __name__ = 'Greedy'
+
+    def __init__(self, Q):
+        self.Q = Q
+
+    def next_action(self, s, r):
+        return np.argmax(expected_value(self.Q[:, s.y, s.x]))
+
+    def reset(self):
+        pass
+
+
+class NaiveCvar(Policy):
+    __name__ = 'Naive CVaR'
+
+    def __init__(self, Q, alpha):
+        self.Q = Q
+        self.alpha = alpha
+
+    def reset(self):
+        pass
+
+    def next_action(self, s, r):
+        action_distributions = self.Q[:, s.y, s.x]
+        a = np.argmax([d.cvar(self.alpha) for d in action_distributions])
+        return a
+
+
+class AlphaBasedPolicy(Policy):
+    __name__ = 'alpha-based CVaR'
+
+    def __init__(self, Q, alpha):
+        self.Q = Q
+        self.init_alpha = alpha
+        self.alpha = alpha
+        self.var = None
+
+    def next_action(self, s, r):
+        action_distributions = self.Q[:, s.y, s.x]
+        old_action = np.argmax(expected_value(action_distributions))
+
+        if self.alpha > 0.999:
+            return old_action
+
+        if self.var is not None:
+            self.alpha = action_distributions[old_action].alpha_from_var(min(0, (self.var - r)/gamma))
+
+        # XXX: same cvar => problem
+        # TODO: deal with this?
+        cvars = np.array([d.cvar(self.alpha) for d in action_distributions])
+        if len(np.unique(cvars)) == 1:
+            a = old_action
+        else:
+            a = np.argmax([d.cvar(self.alpha) for d in action_distributions])
+        self.var = action_distributions[a].var(self.alpha)
+
+        # print('s: {}, r: {}, alpha: {}, var:{}'.format(s, r, self.alpha, self.var))
+        # self.Q[a, s.y, s.x].plot()
+
+        # return np.argmax(expected_value(Q[:, s.y, s.x]))
+        return a
+
+    def reset(self):
+        self.alpha = self.init_alpha
+        self.var = None
+
+
 # ===================== algorithms
 def expected_value(rv):
     return rv.expected_value()
@@ -62,7 +190,6 @@ def policy_iteration():
         expvals = expected_value(Q)
         Q_ = eval_fixed_policy(np.argmax(expvals, axis=0))
 
-        # if np.all(np.argmax(expected_value(Q), axis=0) == np.argmax(expected_value(Q_), axis=0)) and i != 0:
         if converged(Q, Q_) and i != 0:
             print("policy fully learned after %d iterations" % (i,))
             break
@@ -87,7 +214,6 @@ def eval_fixed_policy(P):
     while True:
         Q_ = value_update(Q, P)
         if converged(Q, Q_) and i != 0:
-        # if i > 100:
             break
         Q = Q_
         i += 1
@@ -143,25 +269,48 @@ def q_to_v(Q, policy):
     return Vnew
 
 
+def gather_pi_stats(*args, nb_epochs=1000):
+
+    for policy in args:
+        rewards = np.zeros(nb_epochs)
+
+        for i in range(nb_epochs):
+            # if i % 400 == 0:
+            #     print('epoch:', i)
+            S, A, R = epoch(initial_state, policy)
+            policy.reset()
+            rewards[i] = np.sum(R)
+
+        print('----------------')
+        print(policy.__name__)
+        print('expected value=', np.mean(rewards))
+        var, cvar = cvar_from_samples(rewards, alpha)
+        print('cvar_{}={}'.format(alpha, cvar))
+        print('----------------')
+
+
 # evaluates a single epoch starting at start_state, using a policy which can use
 # an action-value function Q as a parameter
 # returns a triple: states visited, actions taken, rewards taken
-def epoch(start_state, policy, Q, max_iters=100):
+def epoch(start_state, policy, max_iters=1000):
     s = start_state
     S = [s]
     A = []
     R = []
     i = 0
+    r = 0
     while s not in goal_states and i < max_iters:
-        a = policy_sample(policy, s, Q)
+        a = policy.next_action(s, r)
         A.append(a)
         trans = transitions(s)[a]
         state_probs = [tran.prob for tran in trans]
         t = trans[np.random.choice(len(trans), p=state_probs)]
 
-        R.append(t.reward)
-        S.append(t.state)
+        r = t.reward
         s = t.state
+
+        R.append(r)
+        S.append(s)
         i += 1
 
     return S, A, R
@@ -169,4 +318,13 @@ def epoch(start_state, policy, Q, max_iters=100):
 
 if __name__ == '__main__':
     Q = policy_iteration()
-    show_results(initial_state, greedy_policy, expected_value(Q))
+
+    greedy_policy = GreedyPolicy(Q)
+
+    alpha = 0.01
+    alpha_policy = AlphaBasedPolicy(Q, alpha=alpha)
+    naive_cvar_policy = NaiveCvar(Q, alpha=alpha)
+
+    gather_pi_stats(greedy_policy, alpha_policy, naive_cvar_policy)
+
+    # show_results(initial_state, exp_model.greedy_policy, expected_value(Q))
