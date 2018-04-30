98b7e08510627ee32db30b77f5a95a16152c3bfa
silvicek <stanksil@fel.cvut.cz>
2018-04-30 17:46:10 +0200

    code cleanup

--- cvar/dqn/ice_lake/icelake.py
+++ cvar/dqn/ice_lake/icelake.py
@@ -6,52 +6,14 @@ from ple.games.base.pygamewrapper import PyGameWrapper
 
 from pygame.constants import K_w, K_a, K_s, K_d
 from ple.games.primitives import Player, Creep
-from ple.games.utils.vec2d import vec2d
 from ple.games.utils import percent_round_int
 
+BG_COLOR = (255, 255, 255)
 
-class PuckCreep(pygame.sprite.Sprite):
 
-    def __init__(self, pos_init, attr, SCREEN_WIDTH, SCREEN_HEIGHT):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.pos = vec2d(pos_init)
-        self.attr = attr
-        self.SCREEN_WIDTH = SCREEN_WIDTH
-        self.SCREEN_HEIGHT = SCREEN_HEIGHT
-
-        image = pygame.Surface(
-            (self.attr["radius_outer"] * 2,
-             self.attr["radius_outer"] * 2))
-        image.fill((0, 0, 0, 0))
-        image.set_colorkey((0, 0, 0))
-        pygame.draw.circle(
-            image,
-            self.attr["color_outer"],
-            (self.attr["radius_outer"], self.attr["radius_outer"]),
-            self.attr["radius_outer"],
-            0
-        )
-
-        image.set_alpha(int(255 * 0.75))
-
-        pygame.draw.circle(
-            image,
-            self.attr["color_center"],
-            (self.attr["radius_outer"], self.attr["radius_outer"]),
-            self.attr["radius_center"],
-            0
-        )
-
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.center = pos_init
-
-    def update(self, ndx, ndy, dt):
-        self.pos.x += ndx * self.attr['speed'] * dt
-        self.pos.y += ndy * self.attr['speed'] * dt
-
-        self.rect.center = (self.pos.x, self.pos.y)
+# class GameObject:
+#
+#     def __init__(self, position, radius, color):
 
 
 class IceLake(PyGameWrapper):
@@ -66,44 +28,22 @@ class IceLake(PyGameWrapper):
         Screen height, recommended to be same dimension as width.
 
     """
+    actions = {
+        "up": K_w,
+        "left": K_a,
+        "right": K_d,
+        "down": K_s
+    }
 
     def __init__(self, width=256, height=256):
 
-        actions = {
-            "up": K_w,
-            "left": K_a,
-            "right": K_d,
-            "down": K_s
-        }
+        PyGameWrapper.__init__(self, width, height, actions=self.actions)
 
-        PyGameWrapper.__init__(self, width, height, actions=actions)
-
-        self.CREEP_BAD = {
-            "radius_center": percent_round_int(width, 0.047),
-            "radius_outer": percent_round_int(width, 0.265),
-            "color_center": (0, 110, 255),
-            "color_outer": (0, 110, 255),
-            "speed": 0.05 * width,
-            "init_position": (self.width/2, self.height/2)
-        }
-
-        self.CREEP_GOOD = {
-            "radius": percent_round_int(width, 0.047),
-            "color": (40, 140, 40),
-            "init_position": (self.width-10, 0 + 10)
-        }
-
-        self.AGENT_COLOR = (60, 60, 140)
-        self.AGENT_SPEED = 0.2 * width
-        self.AGENT_RADIUS = percent_round_int(width, 0.047)
-        self.AGENT_INIT_POS = (0 + 5, 230)
-
-        self.BG_COLOR = (255, 255, 255)
         self.dx = 0
         self.dy = 0
         self.ticks = 0
 
-        self.game_ended = False
+        self._game_ended = False
 
         self.rewards = {
             "tick": -1./30,
@@ -115,6 +55,8 @@ class IceLake(PyGameWrapper):
     def _handle_player_events(self):
         self.dx = 0.0
         self.dy = 0.0
+
+        agent_speed = 0.1 * self.width
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -124,16 +66,16 @@ class IceLake(PyGameWrapper):
                 key = event.key
 
                 if key == self.actions["left"]:
-                    self.dx -= self.AGENT_SPEED
+                    self.dx -= agent_speed
 
                 if key == self.actions["right"]:
-                    self.dx += self.AGENT_SPEED
+                    self.dx += agent_speed
 
                 if key == self.actions["up"]:
-                    self.dy -= self.AGENT_SPEED
+                    self.dy -= agent_speed
 
                 if key == self.actions["down"]:
-                    self.dy += self.AGENT_SPEED
+                    self.dy += agent_speed
 
     def getGameState(self):
         """
@@ -160,41 +102,47 @@ class IceLake(PyGameWrapper):
         """
             Return bool if the game has 'finished'
         """
-        return self.game_ended
+        return self._game_ended
 
     def init(self):
         """
             Starts/Resets the game to its initial state
         """
-
-        self.player = Player(
-            self.AGENT_RADIUS,
-            self.AGENT_COLOR,
-            self.AGENT_SPEED,
-            self.AGENT_INIT_POS,
-            self.width,
-            self.height)
-
-        self.playerGroup = pygame.sprite.GroupSingle(self.player)
-
+        target_radius = percent_round_int(self.width, 0.047)
         self.target = Creep(
-            self.CREEP_GOOD['color'],
-            self.CREEP_GOOD['radius'],
-            self.CREEP_GOOD['init_position'],
-            (1, 1),
-            0.0,
-            1.0,
-            "GOOD",
-            self.width,
-            self.height,
-            0.0  # jitter
+            color=(40, 140, 40),
+            radius=target_radius,
+            pos_init=(self.width-target_radius, target_radius),
+            dir_init=(1, 1),
+            speed=0.0,
+            reward=1.0,
+            TYPE="-",
+            SCREEN_WIDTH=self.width,
+            SCREEN_HEIGHT=self.height,
+            jitter_speed=0.0
         )
 
-        self.ice = PuckCreep(
-            self.CREEP_BAD['init_position'],
-            self.CREEP_BAD,
-            self.screen_dim[0] * 0.75,
-            self.screen_dim[1] * 0.75)
+        self.ice = Creep(
+            color=(0, 110, 255),
+            radius=percent_round_int(self.width, 0.265),
+            pos_init=(self.width/2, self.height/2),
+            dir_init=(1, 1),
+            speed=0.0,
+            reward=1.0,
+            TYPE="-",
+            SCREEN_WIDTH=self.width,
+            SCREEN_HEIGHT=self.height,
+            jitter_speed=0.0
+        )
+        player_radius = percent_round_int(self.width, 0.047)
+        self.player = Player(
+            radius=player_radius,
+            color=(60, 60, 140),
+            speed=0.2 * self.width,
+            pos_init=(1+player_radius, self.height-1-player_radius),
+            SCREEN_WIDTH=self.width,
+            SCREEN_HEIGHT=self.height)
+        self.playerGroup = pygame.sprite.GroupSingle(self.player)
 
         self.creeps = pygame.sprite.Group()
         self.creeps.add(self.target)
@@ -204,7 +152,7 @@ class IceLake(PyGameWrapper):
         self.ticks = 0
         self.lives = -1
 
-        self.game_ended = False
+        self._game_ended = False
 
     def step(self, dt):
         """
@@ -212,7 +160,7 @@ class IceLake(PyGameWrapper):
         """
         dt /= 1000.0
         self.ticks += 1
-        self.screen.fill(self.BG_COLOR)
+        self.screen.fill(BG_COLOR)
 
         self.score += self.rewards["tick"]
 
@@ -222,6 +170,14 @@ class IceLake(PyGameWrapper):
         if self.wall_collide():
             self.score += self.rewards['wall']
 
+        # if vec2d_distance(self.ice.pos, self.player.pos) < self.ice.radius:
+        #     print(vec2d_distance(self.ice.pos, self.player.pos), self.ice.radius)
+        #     if np.random.rand() < 0.01:
+        #         self.game_ended = True
+        #         self.score += self.rewards['loss']
+        # elif vec2d_distance(self.target.pos, self.player.pos) < self.target.radius:
+        #     self.game_ended = True
+        #     self.score += self.rewards['win']
         if pygame.sprite.spritecollide(self.ice, self.playerGroup, False):  # TODO: investigate weird collisions
             if np.random.rand() < 0.1:
                 self.game_ended = True
@@ -229,6 +185,8 @@ class IceLake(PyGameWrapper):
         elif pygame.sprite.spritecollide(self.target, self.playerGroup, False):
             self.game_ended = True
             self.score += self.rewards['win']
+        # print(vec2d_distance(self.target.pos, self.player.pos))
+        # print((self.target.pos.x, self.target.pos.y), (self.player.pos.x, self.player.pos.y))
 
         self.player.draw(self.screen)
         self.creeps.draw(self.screen)
@@ -236,8 +194,12 @@ class IceLake(PyGameWrapper):
     def wall_collide(self):
         x = self.player.pos.x
         y = self.player.pos.y
-        return x <= 0 or x >= self.width - self.AGENT_RADIUS * 2 or \
-               y <= 0 or y >= self.height - self.AGENT_RADIUS * 2
+        return x <= 0 or x >= self.width - self.player.radius * 2 or \
+               y <= 0 or y >= self.height - self.player.radius * 2
+
+
+def vec2d_distance(a, b):
+    return np.sqrt((a.x-b.x)**2 + (a.y-b.y)**2)
 
 
 if __name__ == "__main__":
@@ -247,9 +209,10 @@ if __name__ == "__main__":
     game.screen = pygame.display.set_mode(game.getScreenDims(), 0, 32)
     game.clock = pygame.time.Clock()
     game.rng = np.random.RandomState(24)
-    game.init()
 
     while True:
-        dt = game.clock.tick_busy_loop(60)
-        game.step(dt)
-        pygame.display.update()
+        game.init()
+        while not game.game_over():
+            dt = game.clock.tick_busy_loop(30)
+            game.step(dt)
+            pygame.display.update()
