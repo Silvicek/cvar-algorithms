34c963018239bcd000569617ef4968f7ed27576f
silvicek <stanksil@fel.cvut.cz>
2018-05-07 18:51:32 +0200

    common cvar

---
+++ cvar/common/cvar_computation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def var_cvar_from_samples(samples, alpha):
+    samples = np.sort(samples)
+    alpha_ix = int(np.round(alpha * len(samples)))
+    var = samples[alpha_ix - 1]
+    cvar = np.mean(samples[:alpha_ix])
+    return var, cvar
